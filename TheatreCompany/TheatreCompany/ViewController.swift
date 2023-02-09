0601ccfa15abd43ee4b8972b17ea523fe105320b
Jess Le <jle@inspirato.com>
2023-02-09 10:10:37 -0700

    Refactors calculating amount

--- TheatreCompany/TheatreCompany/ViewController.swift
+++ TheatreCompany/TheatreCompany/ViewController.swift
@@ -67,21 +67,25 @@ class ViewController: UIViewController {
             let play = plays.play
             var thisAmount = 0
 
-            switch play.type {
-            case "tragedy":
-                thisAmount = 40000
-                if perf.audience > 30 {
-                    thisAmount += 1000 * (perf.audience - 30)
+            let calculateAmount: () = {
+                switch play.type {
+                case "tragedy":
+                    thisAmount = 40000
+                    if perf.audience > 30 {
+                        thisAmount += 1000 * (perf.audience - 30)
+                    }
+                case "comedy":
+                    thisAmount = 30000
+                    if perf.audience > 20 {
+                        thisAmount += 10000 + 500 * (perf.audience - 20)
+                    }
+                    thisAmount += 300 * (perf.audience)
+                default:
+                    fatalError("unknown type: \(play.type)")
                 }
-            case "comedy":
-                thisAmount = 30000
-                if perf.audience > 20 {
-                    thisAmount += 10000 + 500 * (perf.audience - 20)
-                }
-                thisAmount += 300 * (perf.audience)
-            default:
-                fatalError("unknown type: \(play.type)")
-            }
+            }()
+
+            calculateAmount
 
             volumeCredits += max(perf.audience - 30, 0)
             if play.type == "comedy" {
